a2e04238a7c6a7a7d748106af9aebde5b6cb7c80
DainisL <dainis186@gmail.com>
2014-08-22 18:41:03 +0300

    add reset method for tests

--- lib/geoip2.rb
+++ lib/geoip2.rb
@@ -44,7 +44,7 @@ module Geoip2
     # @return an instance of Geoip2::Client
     #
     def client
-      @client = Geoip2::Client.new({
+      @client ||= Geoip2::Client.new({
           host: self.host || 'geoip.maxmind.com',
           base_path: self.base_path || '/geoip/v2.0',
           parallel_requests: self.parallel_requests || 5,
@@ -54,12 +54,17 @@ module Geoip2
     end
 
     private
+    attr_reader :geoip_client
 
     #
     # executes any function on the Geoip2::Client instance
     #
     # @param args [*] any argument that we want to pass to the client function
     # @param block [Block] any block that is passed to the client function
+    def reset_client
+      @client = nil
+    end
+
     def method_missing(method_name, *args, &block)
       return super unless client.respond_to?(method_name)
       client.send(method_name, *args, &block)
--- spec/support/helper.rb
+++ spec/support/helper.rb
@@ -1,6 +1,7 @@
 RSpec.configure do |config|
   config.before do |example|
     Geoip2.configure do |conf|
+      Geoip2.send(:reset_client)
       conf.license_key = ENV['LICENSE_KEY']
       if example.metadata[:version]
         conf.base_path = "/geoip/v#{example.metadata[:version]}"
